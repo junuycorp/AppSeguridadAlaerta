2d7150815c8c9d6ee930cecf5fdce531617b9cbb
Amaru Villasante <avillasanteleon@gmail.com>
2023-11-15 23:20:22 -0500

    Actualizacion workflows

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,13 @@ module.exports = {
     {
       name: `alerta-ciudadano-back-5926`,
       script: 'yarn',
-      args: 'deploy',
+      args: 'deploy:ciudadano',
       interpreter: 'node@20.9.0',
     },
     {
       name: `alerta-agente-back-5927`,
       script: 'yarn',
-      args: 'deploy',
+      args: 'deploy:agente',
       interpreter: 'node@20.9.0',
     },
   ],
