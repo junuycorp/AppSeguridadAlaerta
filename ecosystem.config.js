7b8fd227921cd83e2c3034edcaa0ef7736b1c401
Amaru Villasante <avillasanteleon@gmail.com>
2023-11-20 04:19:48 -0500

    Actualización despliegue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,13 @@ module.exports = {
     {
       name: `alerta-ciudadano-back-5926`,
       script: 'yarn',
-      args: 'deploy:ciudadano',
+      args: 'start:ciudadano',
       interpreter: 'node@20.9.0',
     },
     {
       name: `alerta-agente-back-5927`,
       script: 'yarn',
-      args: 'deploy:agente',
+      args: 'start:agente',
       interpreter: 'node@20.9.0',
     },
   ],
