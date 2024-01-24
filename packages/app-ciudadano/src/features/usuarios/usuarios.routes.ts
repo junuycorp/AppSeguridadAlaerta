c6b1213882dac68ed3dd8c011dc30eb3ee98859e
AmaruVL <avillasanteleon@gmail.com>
2024-01-24 11:25:13 -0500

    Push token added

--- packages/app-ciudadano/src/features/usuarios/usuarios.routes.ts
+++ packages/app-ciudadano/src/features/usuarios/usuarios.routes.ts
@@ -1,18 +1,23 @@
 import { Router } from 'express'
-import {
-  actualizar,
-  buscar,
-  // eliminar,
-  listar,
-  crear,
-  cambiarEstado,
-} from './crud-usuario/crud-usuario.controller'
+import // actualizar,
+// buscar,
+// // eliminar,
+// listar,
+// crear,
+// cambiarEstado,
+'./crud-usuario/crud-usuario.controller'
+import { pushToken } from './push-token/push-token.controller'
 
-export const mantenimientos = Router()
+const mantenimientos = Router()
 
-mantenimientos.get('/listar', listar)
-mantenimientos.get('/buscar/:nroDocumento', buscar)
-mantenimientos.post('/crear', crear)
-mantenimientos.put('/actualizar/:nroDocumento', actualizar)
-mantenimientos.patch('/cambiar-estado/:nroDocumento', cambiarEstado)
+// mantenimientos.get('/listar', listar)
+// mantenimientos.get('/buscar/:nroDocumento', buscar)
+// mantenimientos.post('/crear', crear)
+// mantenimientos.put('/actualizar/:nroDocumento', actualizar)
+// mantenimientos.patch('/cambiar-estado/:nroDocumento', cambiarEstado)
+
 // mantenimientos.delete('/eliminar/:nroDocumento', eliminar)
+
+mantenimientos.patch('/actualizar/push-token', pushToken)
+
+export default mantenimientos
