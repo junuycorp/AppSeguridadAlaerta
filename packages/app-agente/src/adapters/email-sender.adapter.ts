a08d3fdeea6bf371cad0178b9bccfaec09e654a1
Amaru Villasante <72510329+AmaruVL@users.noreply.github.com>
2024-05-08 13:49:17 -0500

    Change email service (#51)
    * Change email provider
    
    * Change email service

--- packages/app-agente/src/adapters/email-sender.adapter.ts
+++ packages/app-agente/src/adapters/email-sender.adapter.ts
@@ -3,18 +3,21 @@ import { createTransport } from 'nodemailer'
 
 const MAILER_EMAIL = envs.MAILER_EMAIL
 const MAILER_PASS = envs.MAILER_PASS
+const MAILER_HOST = envs.MAILER_HOST
+const MAILER_PORT = envs.MAILER_PORT
 
 const transporter = createTransport({
-  service: 'gmail',
-  port: 465,
+  // service: 'gmail',
+  host: MAILER_HOST,
+  port: MAILER_PORT,
   secure: true,
   auth: {
     user: MAILER_EMAIL,
     pass: MAILER_PASS,
   },
-  tls: {
-    rejectUnauthorized: true,
-  },
+  // tls: {
+  //   rejectUnauthorized: true,
+  // },
 })
 
 const sendEmail = async (
